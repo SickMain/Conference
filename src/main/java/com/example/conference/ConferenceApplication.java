78c3c455d7ca506b5457308fdbec61113096cada
SickMain <Kirya.rybyakov.03@mail.ru>
2023-10-08 12:39:31 +0800

    checking stuff

--- src/main/java/com/example/conference/ConferenceApplication.java
+++ src/main/java/com/example/conference/ConferenceApplication.java
@@ -1,5 +1,6 @@
 package com.example.conference;
 
+import com.example.conference.repository.RoleRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
