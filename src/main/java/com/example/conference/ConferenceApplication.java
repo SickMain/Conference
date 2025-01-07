7ed028423354b3bf80b827a32ba98d0d09e4fae7
SickMain <Kirya.rybyakov.03@mail.ru>
2025-01-08 05:15:06 +0800

    Небольшие правки

--- src/main/java/com/example/conference/ConferenceApplication.java
+++ src/main/java/com/example/conference/ConferenceApplication.java
@@ -1,6 +1,5 @@
 package com.example.conference;
 
-import com.example.conference.repository.RoleRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
